dc1d8f98c14e6ff7b3fd29af55a4d3cde0a9530a
Jo Costa <joaldocosta97@gmail.com>
2024-07-31 08:46:19 +0100

    example profile pic

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -17,7 +17,7 @@
         </p>
       </div>
       <div class="img-container">
-        <img class="" src="../assets/profilepic.jpg" alt="" srcset="" width="" />
+        <img class="" src="../assets/profile.jpg" alt="" srcset="" width="" />
       </div>
     </div>
     <div class="w-4/5 flex flex-col justify-start m-auto mt-8 text-left align-text md:text-1xl">
